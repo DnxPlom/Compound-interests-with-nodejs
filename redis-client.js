f19aefaa2cbc6c6b3ad4a284595ffdb67afb6986
Dan Tuwei <32453566+DnxPlom@users.noreply.github.com>
2022-01-15 23:57:31 +0300

    Update redis-client.js

--- redis-client.js
+++ redis-client.js
@@ -1,9 +1,16 @@
 const redis = require('redis');
 
-const client = redis.createClient();
+const redisPort = process.env.REDIS_PORT ? process.env.REDIS_PORT : 6379;
+const redisHost = process.env.REDIS_HOST ? process.env.REDIS_HOST : 'localhost';
+
+const url = `redis://${redisHost}:${redisPort}`;
+
+const client = redis.createClient({
+    url
+});
 
 client.on('error', (err) => console.log('Redis Client Error', err));
-  
+
 (async () => {
     await client.connect();
 })();
